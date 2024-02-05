cca768b9b4a63c3606a9ea56acf2f0a9a84a86f8
Serhii Tarasenko <stdev33@gmail.com>
2024-02-05 15:30:44 +0200

    [+] task 3. FriendList

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,10 @@
 import Profile from 'components/Profile/Profile';
 import ProfileCSS from './Profile/Profile.module.css';
 import Statistics from 'components/Statistics/Statistics';
+import FriendList from 'components/FriendList/FriendList';
 import user from 'data/user.json';
 import data from 'data/data.json';
+import friends from 'data/friends.json';
 
 export const App = () => {
   return (
@@ -26,6 +28,7 @@ export const App = () => {
       />
       <Statistics title="Upload stats" stats={data} />
       <Statistics stats={data} />
+      <FriendList friends={friends} />;
     </div>
   );
 };
